bb8c4b8648d8e820a02f12cf0e26fccd4c432f59
Abinaya U <abinaya.u@thoughtworks.com>
2021-05-01 16:44:08 +0530

    Add. Filter function

---
+++ src/filter.js
@@ -0,0 +1,15 @@
+const isUpperCase = require("./isUpperCase");
+
+const filter = (array,functionName) => {
+    
+    const result =[];
+    for(let arrayValue of array)
+    {   
+        if(functionName(arrayValue))
+        result.push(arrayValue)
+    }
+   return result;
+  
+}
+
+module.exports = filter;
---
+++ src/isUpperCase.js
@@ -0,0 +1,7 @@
+
+const isUpperCase = (element) => {
+    return element === element.toUpperCase()? true:false;
+
+}
+
+module.exports = isUpperCase;
