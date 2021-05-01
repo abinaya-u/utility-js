3ad743fbe4b6131c157c94600a3e1b24e7e78a36
Abinaya U <abinaya.u@thoughtworks.com>
2021-05-01 20:12:13 +0530

    Add. Min function

---
+++ src/min.js
@@ -0,0 +1,13 @@
+const min = (array) => {
+
+    let minimum=array[0];
+     for(let element of array)
+     {
+         minimum = (minimum>element)?element:minimum
+     }
+    return minimum;
+   
+ }
+ 
+ module.exports = min;
+ 
---
+++ src/min.spec.js
@@ -0,0 +1,13 @@
+const min = require("./min")
+describe("Max function", () => {
+	it("should return minimum element in the array", () => {
+		expect(min([1,2,3,4])).toEqual(1);
+	});
+    it("should return minimum element in the array even if array contains single element", () => {
+		expect(min([1])).toEqual(1);
+	});
+    it("should return undefined if array contains no element", () => {
+		expect(min([])).toEqual(undefined);
+	});
+   
+});
