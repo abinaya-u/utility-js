90fe107bc2466ba7f4a4be96d21282b76c87ed89
Abinaya U <abinaya.u@thoughtworks.com>
2021-05-01 17:26:53 +0530

    Add. Reduce function

---
+++ src/reduce.js
@@ -0,0 +1,12 @@
+const head = require("./head");
+const tail = require("./tail");
+const reduce = (array, functionName, value) => {
+	while (array.length > 0) {
+		 value = (value)?functionName(value, head(array)) : head(array);
+		array = tail(array);
+	}
+
+	return value;
+};
+
+module.exports = reduce;
---
+++ src/reduce.spec.js
@@ -0,0 +1,15 @@
+const reduce = require("./reduce")
+describe("Filter function", () => {
+	it("should return undefined when [],(x,y)=>x+y is passed", () => {
+		expect(reduce([],(x,y)=>x+y )).toEqual(undefined);
+	});
+    it("should return 10 when [],v(x,y)=>x+y,10 is passed", () => {
+		expect(reduce([],(x,y)=>x+y,10)).toEqual(10);
+    });
+    it("should return 10 when [],v(x,y)=>x+y,10 is passed", () => {
+		expect(reduce(['a','b','c'],(x,y)=>x+y)).toEqual("abc");
+    });
+    it("should return 10 when [],v(x,y)=>x+y,10 is passed", () => {
+		expect(reduce(['a','b','c'],(x,y)=>x+y,'z')).toEqual("zabc");
+    });
+});
